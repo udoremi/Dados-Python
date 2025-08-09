84366e479484e2ca0cf7efc27cf45d7a1b7279d8
Matheus Selis Soares <matheusseliss@gmail.com>
2025-08-09 11:23:38 -0300

    Initial commit

---
+++ correcao.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/vqrca/dashboard_salarios_dados/refs/heads/main/dados-imersao-final.csv")
+
+df['senioridade'] = df['senioridade'].replace({
+    'senior': 'Sênior',
+    'pleno': 'Pleno',
+    'junior': 'Júnior'
+})
+
+df['contrato'] = df['contrato'].replace({
+    'integral': 'Integral',
+    'meio-periodo': 'Meio Período',
+    'freelance': 'Freelance',
+    'temporario': 'Temporário'
+})
+
+df['tamanho_empresa'] = df['tamanho_empresa'].replace({
+    'media': 'Média',
+    'grande': 'Grande',
+    'pequena': 'Pequena'
+})
+
+df['tamanho_empresa'] = df['tamanho_empresa'].replace({
+    'media': 'Média',
+    'grande': 'Grande',
+    'pequena': 'Pequena'
+})
+
+df['remoto'] = df['remoto'].replace({
+    'remoto': 'Remoto',
+    'presencial': 'Presencial',
+    'hibrido': 'Híbrido'
+})
+
+df.to_csv('dados_imersao_corrigidos.csv', index=False)
+
+print("Arquivo com ortografia corrigida salvo como 'dados_corrigidos.csv'")
